42a2e1966a3cc52a7db86ead1c9f3dc6fed5c482
Tevin <tingquanren@163.com>
2021-01-11 12:10:35 +0800

    添加无效bug

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,42 @@
 <template>
-  <view class="index">
-    <text>{{ msg }}</text>
-  </view>
+    <view class="index">
+        <view>
+            <view>checkbox disabled 无效</view>
+            <label>
+                <checkbox disabled>测试</checkbox>
+                <checkbox :disabled="true">测试</checkbox>
+            </label>
+        </view>
+        <view>
+            <view>checkbox group disabled 无效</view>
+            <checkbox-group @change="evt=>null">
+                <label>
+                    <checkbox
+                        :value="1"
+                        :disabled="true"
+                    >组测试1
+                    </checkbox>
+                </label>
+                <label>
+                    <checkbox
+                        :value="2"
+                        :disabled="false"
+                    >组测试1
+                    </checkbox>
+                </label>
+            </checkbox-group>
+        </view>
+    </view>
 </template>
 
 <script>
-import './index.scss'
+import './index.scss';
 
 export default {
-  data () {
-    return {
-      msg: 'Hello world!'
-    }
-  }
-}
+    data() {
+        return {
+            msg: 'Hello world!',
+        };
+    },
+};
 </script>
